bc43c9ee4ea5b78d0845486df2ae897a3c64040f
dan <everkleer80@gmail.com>
2022-08-14 23:11:05 +0000

    Update services, add strings for UI

--- custom_components/super_email/save_email.py
+++ custom_components/super_email/save_email.py
@@ -1,18 +1,20 @@
 import imaplib
 
 
-def save_email(host, port, username, password, folder, filename):
-    gmail = imaplib.IMAP4_SSL(host, port=port)
-    gmail.login(username, password)
-    gmail.select(folder)
-    typ, data = gmail.search(None, "ALL")
+def save_email(host, port, username, password, emailfolder, savefolder, filename):
+    """Save the latest email to a file"""
+    imap = imaplib.IMAP4_SSL(host, port=port)
+    imap.login(username, password)
+    imap.select(emailfolder)
+    typ, data = imap.search(None, "ALL")  # pylint: disable=unused-variable
 
     d = data[0].split()
 
     e = d[len(d) - 1]
 
-    typ, data = gmail.fetch(e, "(RFC822)")
-    f = open("%s/%s" % ("config/uber", filename), "wb")
-    f.write(data[0][1])
-    gmail.close()
-    gmail.logout()
+    typ, data = imap.fetch(e, "(RFC822)")
+    file = open("%s/%s" % (savefolder, filename), "wb")
+    file.write(data[0][1])
+    file.close()
+    imap.close()
+    imap.logout()
