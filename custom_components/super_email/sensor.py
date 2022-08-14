b2961912450f88f1087685384405a6103b5836ae
dan <everkleer80@gmail.com>
2022-08-14 23:11:05 +0000

    Update services, add strings for UI

--- custom_components/super_email/sensor.py
+++ custom_components/super_email/sensor.py
@@ -1,15 +1,13 @@
+"""Sensors for super_email Home Assistant integration"""
 import asyncio
 import email
-from imaplib2 import IMAP4_SSL
+from typing import Any, Optional
 import os
-
 import logging
 from datetime import timedelta
-from threading import *
-from typing import Any, Optional
-
+from threading import Thread, Event
+from imaplib2 import IMAP4_SSL
 import imaplib2
-from sqlalchemy import true
 
 # import pandas as pd
 from homeassistant import config_entries, core
@@ -17,7 +15,9 @@ from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.typing import (
     HomeAssistantType,
 )
-
+from homeassistant.helpers.entity_platform import current_platform
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN
 from .save_email import save_email
@@ -36,20 +36,27 @@ async def async_setup_entry(
     """Setup sensors from a config entry created in the integrations UI."""
     config = hass.data[DOMAIN][config_entry.entry_id]
 
-    def send_emails(call):
+    def send_email(call):
         host = config["smtp_server"]
         port = config["smtp_port"]
         username = config["username"]
         password = config["password"]
+        folder = call.data.get("folder")
+        sender = call.data.get("sender")
+        recipient = call.data.get("recipient")
+        subject = call.data.get("subject")
+        body = call.data.get("body", "")
+        delete_files = call.data.get("delete_files_after_sending")
+
         files = []
-        for filename in os.listdir("config/uber"):
-            files.append("config/uber/" + filename)
+        for filename in os.listdir(folder):
+            files.append(folder + "/" + filename)
 
         send_mail(
-            "Daniel Carter <everkleer80@gmail.com>",
-            "Daniel Carter <everkleer80@gmail.com>",
-            "Uber Expense",
-            "",
+            sender,
+            recipient,
+            subject,
+            body,
             files,
             server=host,
             port=port,
@@ -57,19 +64,23 @@ async def async_setup_entry(
             password=password,
         )
 
-    def save_latest_email(call):
-        folder = call.data.get("folder", "INBOX")
-        filename = call.data.get("filename", "")
+        if delete_files:
+            for file in files:
+                os.remove(file)
 
-        host = config["imap_server"]
-        port = config["imap_port"]
-        username = config["username"]
-        password = config["password"]
-        save_email(host, port, username, password, folder, filename)
+    platform = current_platform.get()
+    platform.async_register_entity_service(
+        "save_latest_email",
+        {
+            vol.Required("save_to_folder"): cv.string,
+            vol.Required("filename"): cv.string,
+        },
+        "save_latest_email",
+    )
 
-    hass.services.async_register(DOMAIN, "send_emails", send_emails)
+    hass.services.async_register(DOMAIN, "send_email", send_email)
 
-    hass.services.async_register(DOMAIN, "save_latest_email", save_latest_email)
+    # hass.services.async_register(DOMAIN, "save_latest_email", save_latest_email)
 
     sensors = [
         EmailSensor(
@@ -95,29 +106,43 @@ class EmailSensor(Entity):
         self._username = username
         self._password = password
         self._folder = folder
+        self._attr_name = host + "_" + folder
 
         self._imap_session: IMAP4_SSL = None
         self._idler: Idler = None
 
-        self._name = "email"
         self._state = None
         self._available = True
-        self._attr_unique_id = username
+        self._attr_unique_id = "super_email_" + host + "_" + folder
 
         self._attrs: dict[str, Any] = {}
 
+    async def save_latest_email(self, save_to_folder, filename):
+        """Save the latest email to a file"""
+        emailfolder = self._folder
+
+        host = self._host
+        port = self._port
+        username = self._username
+        password = self._password
+        save_email(
+            host, port, username, password, emailfolder, save_to_folder, filename
+        )
+
     @property
     def imap_session(self) -> IMAP4_SSL:
+        """IMAP session to get emails from"""
         return self._imap_session
+
+    @property
+    def hass(self) -> HomeAssistantType:
+        """Home Assistant Instance"""
+        return self._hass
 
     @property
     def available(self) -> bool:
         """Return True if entity is available."""
         return self._available
-
-    @property
-    def name(self) -> str:
-        return self._name
 
     @property
     def state(self) -> Optional[str]:
@@ -135,18 +160,20 @@ class EmailSensor(Entity):
         self._imap_session.select(self._folder)
 
     def try_disconnect(self):
+        """Close the current IMAP session, ignore errors"""
         if self._imap_session.state == "SELECTED":
             try:
                 self._imap_session.close()
-            except Exception:
+            except Exception:  # pylint: disable=broad-except
                 pass
         if self._imap_session.state != "LOGOUT":
             try:
                 self._imap_session.logout()
-            except Exception:
+            except Exception:  # pylint: disable=broad-except
                 pass
 
     async def async_try_reconnect(self, wait: int = 0):
+        """Disconnect and reconnect the IMAP session"""
         self._available = False
         self.try_disconnect()
 
@@ -159,7 +186,7 @@ class EmailSensor(Entity):
             self.connect_imap()
             self._available = True
             self.schedule_update_ha_state(True)
-        except Exception as ex:
+        except Exception as ex:  # pylint: disable=broad-except
             _LOGGER.error(ex)
             self.schedule_update_ha_state(False)
             self._hass.async_create_task(
@@ -167,6 +194,7 @@ class EmailSensor(Entity):
             )  # try again in 1 minute
 
     def start_monitor(self):
+        """Start a new thread to monitor emails if one doesn't already exist"""
         if self._imap_session is None:
             self.connect_imap()
 
@@ -176,24 +204,33 @@ class EmailSensor(Entity):
             self._idler.start()
 
     def process_new_email(self):
+        """Called from monitor thread when an IMAP event is received"""
         self.schedule_update_ha_state(True)
 
     async def async_update(self):
+        """Updates the sensor state"""
         _LOGGER.debug("async_update")
         if self._imap_session is None:
             try:
                 self.connect_imap()
-            except Exception as ex:
+            except Exception as ex:  # pylint: disable=broad-except
                 _LOGGER.error(ex)
                 self._available = False
                 await self.async_try_reconnect()
                 return
 
-        typ, data = self._imap_session.search(None, "ALL")
+        typ, data = self._imap_session.search(  # pylint: disable=unused-variable
+            None, "ALL"
+        )
 
         id_list = data[0].split()
 
         latest_id = id_list[len(id_list) - 1]
+
+        sender = ""
+        date = ""
+        subject = ""
+        recipient = ""
 
         typ, msg = self._imap_session.fetch(latest_id, "(RFC822)")
         for response in msg:
@@ -204,25 +241,27 @@ class EmailSensor(Entity):
                 sender = msg_data["From"]
                 subject = msg_data["Subject"]
                 date = msg_data["Date"]
-                to = msg_data["To"]
-
-                self._state = subject
+                recipient = msg_data["To"]
 
         # self._attrs = msg_data
         self._attrs["from"] = sender
         self._attrs["date"] = date
         self._attrs["subject"] = subject
-        self._attrs["to"] = to
+        self._attrs["to"] = recipient
         self._attrs["body_text"] = ""
         self._attrs["body_html"] = ""
+        self._state = subject
 
-        for part in msg_data.walk():
-            if part.get_content_type() == "text/plain":
-                self._attrs["body_text"] = part.get_payload()
-            if part.get_content_type() == "text/html":
-                self._attrs["body_html"] = part.get_payload()
+        if date == "":
+            self._available = False
+        else:
+            for part in msg_data.walk():
+                if part.get_content_type() == "text/plain":
+                    self._attrs["body_text"] = part.get_payload()
+                if part.get_content_type() == "text/html":
+                    self._attrs["body_html"] = part.get_payload()
 
-        self._available = True
+            self._available = True
         self.start_monitor()
 
         # self._hass.async_add_executor_job(self.async_start_monitor)
@@ -231,37 +270,43 @@ class EmailSensor(Entity):
 # This is the threading object that does all the waiting on
 # the event
 class Idler(object):
+    """This is the threading object that does all the waiting on the event"""
+
     def __init__(self, email_sensor: EmailSensor):
         self.thread = Thread(target=self.idle)
         self.email_sensor = email_sensor
         self.event = Event()
+        self.needsync = False
 
     def start(self):
+        """Starts the thread"""
         self.thread.start()
 
     def stop(self):
+        """Ends the thread"""
         # This is a neat trick to make thread end. Took me a
         # while to figure that one out!
         self.event.set()
 
     def join(self):
+        """Join the thread"""
         self.thread.join()
 
     def idle(self):
-        # Starting an unending loop here
+        """Thread loop - Starting an unending loop here"""
         while True:
             # This is part of the trick to make the loop stop
             # when the stop() command is given
             if self.event.isSet():
                 return
-            if not self.email_sensor._available:
+            if not self.email_sensor.available:
                 continue
 
             self.needsync = False
             # A callback method that gets called when a new
             # email arrives. Very basic, but that's good.
             def callback(args):
-                _LOGGER.debug("IMAP event")
+                _LOGGER.debug("IMAP event %s", args)
                 if not self.event.isSet():
                     self.needsync = True
                     self.event.set()
@@ -270,15 +315,14 @@ class Idler(object):
             # since it's asynchronous.
             try:
                 _LOGGER.debug("Waiting for IMAP events")
-                self.email_sensor._imap_session.idle(callback=callback)
+                self.email_sensor.imap_session.idle(callback=callback)
             except IMAP4_SSL.abort as ex:
                 # connection lost, kill thread and attempt restart
                 _LOGGER.error(ex)
                 asyncio.run_coroutine_threadsafe(
                     self.email_sensor.async_try_reconnect(),
-                    self.email_sensor._hass.loop,
+                    self.email_sensor.hass.loop,
                 )
-                self.email_sensor._available = False
                 continue
             # This waits until the event is set. The event is
             # set by the callback, when the server 'answers'
@@ -293,7 +337,6 @@ class Idler(object):
                 self.event.clear()
                 self.dosync()
 
-    # The method that gets called when a new email arrives.
-    # Replace it with something better.
     def dosync(self):
+        """The method that gets called when a new email arrives or the idle event times out"""
         self.email_sensor.process_new_email()
